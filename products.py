b829050863d1a3d6cd3ff1d284b9765d7ed2c409
Priya Guo <priya.guo@gmail.com>
2021-05-20 16:22:20 +0800

    21/05/20

--- products.py
+++ products.py
@@ -1,10 +1,19 @@
+#讀取檔案
+#split 切割
+products = []
+with open('products.csv', 'r') as f:
+    for line in f:
+        name, price = line.strip().split(',')
+        products.append([name, price])
+print(products)
+
 #二維清單
 products = []
 while True:
     name = input('請輸入商品名稱:')
     if name == 'q':
         break
-    price = int(input('請輸入商品價格:'))
+    price = str(input('請輸入商品價格:'))
     #p = []
     #p.append(name)
     #p.append(price)
@@ -18,3 +27,13 @@ for p in products:
 # print(products[0][1])
 # print(products[1][0])
 # print(products[1][1])
+
+# r → read 讀取
+# w → write 寫入
+# open 打開檔案
+# f.write →寫入檔案
+with open('products.csv','w',encoding = 'BIG5') as f:
+    f.write('商品,價格' + '\n')
+    for p in products:
+        f.write(p[0] + ',' + str(p[1]) + '\n')
+
